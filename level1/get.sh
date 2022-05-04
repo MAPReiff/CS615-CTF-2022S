cdc9d49c41e8606938c4f09f5ba11b438d21f88f
Tyler Kaminski <durcor@disroot.org>
2022-05-04 17:29:48 -0400

    Remove trailing period from FQDNs

--- level1/get.sh
+++ level1/get.sh
@@ -4,7 +4,8 @@ checksum="6b79f4baad3e6f562bfd66bc2ba745ad27a5e5f1102e7eb2f98d945ebfc8c8ae"
 
 while read -r ip; do
     # current_sum=$(host "$ip" | awk '{print $5}' | sha256sum | awk '{print $1}')
-    fqdn=$(host "$ip" | awk '{print $NF}')
+    fqdn=$(host "$ip" | awk '{print $NF}' | rev | cut -d. -f2- | rev)
+    # echo "$ip - $fqdn"
     current_sum=$(echo "$fqdn" | sha256sum | awk '{print $1}')
     # echo "$current_sum"
     if [ "$current_sum" = "$checksum" ]; then
