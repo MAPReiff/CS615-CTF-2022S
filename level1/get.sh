479902c34f7d990285aa3b4b11f0b3ea05b51771
Tyler Kaminski <durcor@disroot.org>
2022-05-04 17:17:52 -0400

    Fix fqdn getting echo

--- level1/get.sh
+++ level1/get.sh
@@ -5,7 +5,7 @@ checksum="6b79f4baad3e6f562bfd66bc2ba745ad27a5e5f1102e7eb2f98d945ebfc8c8ae"
 while read -r ip; do
     # current_sum=$(host "$ip" | awk '{print $5}' | sha256sum | awk '{print $1}')
     fqdn=$(host "$ip" | awk '{print $NF}')
-    current_sum=$(echo fqdn | sha256sum | awk '{print $1}')
+    current_sum=$(echo "$fqdn" | sha256sum | awk '{print $1}')
     # echo "$current_sum"
     if [ "$current_sum" = "$checksum" ]; then
         echo "Found it!: $ip : $fqdn"
